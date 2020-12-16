0064f5d3fad37749fbfbf0c752cb2bfc3af81c6d
Million900o <apaolini900o@gmail.com>
2020-12-15 19:10:18 -0500

    fixed bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const pkg = require('./package.json');
 
 module.exports.DB = require('./lib/DB.js');
-module.exports.PoggersDB = require('./lib/Collection.js');
+module.exports.PoggersDB = require('./lib/PoggersDB.js');
 module.exports.version = pkg.version;
 module.exports.Collection = require('./lib/Collection.js');
-module.exports.PoggersEncryptor = require('./lib/Collection.js');
+module.exports.PoggersEncryptor = require('./lib/PoggersEncryptor.js');
