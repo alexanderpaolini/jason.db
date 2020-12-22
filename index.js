7ca3c6a13737e5b7ee5bcc45c83ea7eade8488fd
Million900o <apaolini900o@gmail.com>
2020-12-22 11:23:31 -0500

    added configuration

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const pkg = require('./package.json');
 
 module.exports.DB = require('./lib/DB.js');
-module.exports.PoggersDB = require('./lib/PoggersDB.js');
 module.exports.version = pkg.version;
 module.exports.Collection = require('./lib/Collection.js');
+module.exports.PoggersDB = require('./lib/PoggersDB.js');
+module.exports.FileNameDB = require('./lib/FileNameDB.js');
 module.exports.PoggersEncryptor = require('./lib/PoggersEncryptor.js');
